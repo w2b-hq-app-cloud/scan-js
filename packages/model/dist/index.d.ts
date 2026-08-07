8281df0962f368bdd72a3284f1cd81840fe6e6d7
Aurelian Apostol <aurelian.apostol@gmail.com>
2026-08-07 13:16:04 +0300

    Quote SCAN YAML string values on serialize so colon-rich descriptions round-trip without parse errors.

--- packages/model/dist/index.d.ts
+++ packages/model/dist/index.d.ts
@@ -1,5 +1,5 @@
 export { SCAN_VERSION, SPHERE_VERSION, scanModelSchema, sphereModelSchema, connectionTypeSchema, iconSchema, boundaryColorSchema, type ScanModel, type SphereModel, type SphereView, type SphereComponent, type SphereConnection, type SpherePort, type ElementLink, type ConnectionType, type LayoutEntry, } from "./schema.js";
 export { parseScanYaml, parseScanJson, parseSphereYaml, parseSphereJson, } from "./parse.js";
-export { serializeScanYaml, serializeScanJson, serializeSphereYaml, serializeSphereJson, } from "./serialize.js";
+export { serializeScanYaml, serializeScanJson, serializeSphereYaml, serializeSphereJson, scanStringNeedsQuotes, } from "./serialize.js";
 export { createEmptyModel, createEmptySphereModel, slugifyId, type CreateEmptyModelOptions, } from "./empty.js";
 export { validateScanModel, validateSphereModel, type ScanValidationIssue, type ScanValidationResult, type ScanValidationSeverity, } from "./validate.js";
