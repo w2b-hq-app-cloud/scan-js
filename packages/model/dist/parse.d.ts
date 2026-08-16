793038f7e98b3d988c2c2cc6dd8c9bd5808bdee4
Sphere <sphere-local@localhost>
2026-08-16 21:41:41 +0300

    chore(sphere): build component-build-1786880210083

--- packages/model/dist/parse.d.ts
+++
@@ -1,7 +0,0 @@
-import { type ScanModel } from "./schema.js";
-export declare function parseScanYaml(source: string): ScanModel;
-export declare function parseScanJson(source: string | unknown): ScanModel;
-/** @deprecated Use parseScanYaml */
-export declare const parseSphereYaml: typeof parseScanYaml;
-/** @deprecated Use parseScanJson */
-export declare const parseSphereJson: typeof parseScanJson;
--- packages/modeler/dist/index.js
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright 2026 WABLOO PARTNERS SRL
-export { CommandStack } from "./command-stack.js";
-export { Modeling, syncBoundaryMembership, nodeKindToCreateKind, } from "./modeling.js";
-export { computeAutoLayout, } from "./auto-layout.js";
-export { mergeModels } from "./merge.js";
-export { ScanModeler, SphereModeler } from "./modeler.js";
-export { default } from "./modeler.js";
